87cecfa82fa8ae0c8a3032b5606a3910eaa87d0e
TheDarkViking <mickoswag25@gmail.com>
2023-06-27 20:44:34 +0800

    6/27/2023 - 8:44 pm update -micko

--- UI.java
+++ UI.java
@@ -1,6 +1,26 @@
-public class UI {
-    public static void main (String[]args)
+import javax.swing.*;
+
+public class UI extends JFrame {
+    private JPanel panel1;
+    private JPanel panelMain;
+    private JTable table1;
+    private JComboBox procNo;
+    private JButton simulateButton;
+    private JTextField textField1;
+    private JLabel process;
+
+    public UI ()
     {
-        System.out.println("UI MESSAGE COMMIT");
+        setContentPane(panel1);
+        setTitle("CPU Scheduling");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700,300);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new UI();
     }
 }
