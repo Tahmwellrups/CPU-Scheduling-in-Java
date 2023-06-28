e590ed28a8c576374570ac969bd3d9b2a6a8ceee
Tahmwellrups <ramwell.tupas@gmail.com>
2023-06-28 22:05:44 +0800

    10:04 PM (06/28/2023) Complete output of processes

--- src/PreempPriority.java
+++ src/PreempPriority.java
@@ -18,16 +18,17 @@ class Process {
     }
 }
 
-class PreemptivePriority{
-    public static void preemptivePriority(LinkedList<Data> data){
+class PreemptivePriority {
+    public static void preemptivePriority(LinkedList<Data> data) {
         List<Process> processes = new ArrayList<>();
-        int[] burstTimes = new int[data.size()];
+        List<Integer> burstTimes = new ArrayList<>();
 
-        // Add processes to the list and store burst times in the array
-        for (int i = 0; i < data.size(); i++) {
-            Data proc = data.get(i);
-            processes.add(new Process(proc.name, proc.at, proc.bt, proc.pt));
-            burstTimes[i] = proc.bt;
+        // Add processes to the list and store burst times in each process
+        for (Data proc : data) {
+            Process process = new Process(proc.name, proc.at, proc.bt, proc.pt);
+            process.burstTime = proc.bt; // Store the burst time directly in the Process object
+            processes.add(process);
+            burstTimes.add(proc.bt); // Store the burst time in the burstTimes list
         }
 
         // Sort the processes based on arrival time (if needed)
@@ -73,13 +74,12 @@ class PreemptivePriority{
         }
 
         // Calculate and display the response time, waiting time, and turnaround time for each process
-        System.out.println("\n\nProcess\t\tPriority\tBurst Time\tStart Time\tEnd Time\tWaiting Time\tTurnaround Time");
-        for (Process process : completedProcesses)
-        {
-
+        System.out.println("\n\nProcess\t\tPriority\tArrival Time\tBurst Time\tStart Time\tEnd Time\tWaiting Time\tTurnaround Time");
+        for (int i = 0; i < completedProcesses.size(); i++) {
+            Process process = completedProcesses.get(i);
             int waitingTime = process.startTime - process.arrivalTime;
             int turnaroundTime = process.endTime - process.arrivalTime;
-            System.out.println(process.name + "\t\t\t" + process.priority + "\t\t\t" + burstTimes[data.indexOf()] + "\t\t\t" +
+            System.out.println(process.name + "\t\t\t" + process.priority + "\t\t\t" + process.arrivalTime + "\t\t\t\ta" + burstTimes.get(i) + "\t\t\t" +
                     process.startTime + "\t\t\t" + process.endTime + "\t\t\t" + waitingTime
                     + "\t\t\t\t" + turnaroundTime);
         }
