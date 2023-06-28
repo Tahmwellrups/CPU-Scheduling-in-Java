24f3693638c04649a7835f15b94e5aed1e7b7f75
Hendrickson Nifas <nifashendrickson@gmail.com>
2023-06-29 01:15:32 +0800

    inputs are now from user

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,16 +1,31 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class RoundRobin {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         // Create a queue of processes
         Queue<Process> queue = new LinkedList<>();
-        // create a queue for display table
 
-        // Add processes to the queue (name, arrivalTime, burstTime)
+        System.out.print("Number of Process: ");
+        int numProcess = scan.nextInt();
+
+        for (int i = 0; i < numProcess; i++) {
+            String name = "P" + (i+1);
+            System.out.print("Enter arrival time for " + name + ": ");
+            int arrivalTime = scan.nextInt();
+            System.out.print("Enter burstTime for " + name + ": ");
+            int bursTime = scan.nextInt();
+
+            queue.add(new Process(name, arrivalTime, bursTime));
+        }
+
+        /*// Add processes to the queue (name, arrivalTime, burstTime)
         queue.add(new Process("P1", 0, 24));
         queue.add(new Process("P2", 1, 3));
-        queue.add(new Process("P3", 2, 3));
+        queue.add(new Process("P3", 2, 3));*/
 
         // Set time quantum for Round Robin
         int timeQuantum = 4;
