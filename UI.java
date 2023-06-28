ab8aa0209c6c5c7174e703cb9b2a97ea737974c0
TheDarkViking <mickoswag25@gmail.com>
2023-06-28 19:24:02 +0800

    6/28/2023 - 7:23 pm update -micko

--- UI.java
+++ UI.java
@@ -8,14 +8,26 @@ public class UI extends JFrame {
     private JButton simulateButton;
     private JTextField textField1;
     private JLabel process;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
 
     public UI ()
     {
         setContentPane(panel1);
         setTitle("CPU Scheduling");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(700,300);
-        setResizable(false);
+        setSize(700,500);
+        setResizable(true);
         setLocationRelativeTo(null);
         setVisible(true);
     }
